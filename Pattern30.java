d8ba5dd4b2c36ceaef492e204b65f192b4cb3a6f
CHANDRASEKHAR NALLAMANGALA <99155554+chandrasekharnallamangala@users.noreply.github.com>
2024-06-18 23:05:29 +0530

    Add files via upload

---
+++ Pattern30.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Pattern30 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<i; j++) {
+                System.out.print(" ");
+            }
+
+
+
+        for (int k=0; k<n-i; k++) {
+
+                if(k==0 || k==n-i-1 || i==0) {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
